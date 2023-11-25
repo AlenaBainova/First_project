6aac1570fb3d3620b118479292b4facd5e18f1b4
Alena <alyonabainova2003@gmail.com>
2023-11-25 13:32:08 +0700

    7_var

---
+++ src/EntityListener/ArticleEntityListener.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EntityListener;
+
+use App\Entity\Article;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Event\PrePersistEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Bundle\SecurityBundle\Security;
+
+#[AsEntityListener(event: Events::prePersist, method: 'prePersist', entity: Article::class)]
+
+class ArticleEntityListener
+{
+    public function __construct(
+        private readonly Security $security)
+    {
+    }
+
+
+
+    public function prePersist(PrePersistEventArgs $entity): void
+    {
+       /** @var Article $entity */
+       $entity = $entity->getObject();
+       $entity->setCreatedAt(new \DateTimeImmutable());
+       $entity->setAuthor($this->security->getUser());
+    }
+}
